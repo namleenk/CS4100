8c02486937dec0954784849ecc757c20a2733d31
namleenk <knamleen@gmail.com>
2023-04-25 14:30:32 -0400

    Added argparse to allow the user to compare the search algorithsm in 3 different ways

--- final project/search.py
+++ final project/search.py
@@ -1,4 +1,4 @@
-#from snake2 import snake #, visualize_search
+import argparse
 import random
 from snake import manhattanHeuristic, nullHeuristic, snake, euclideanHeuristic
 from util import Stack, Queue, PriorityQueue
@@ -62,8 +62,8 @@ def dfs(snake: snake):
         average_length = total_length / num_paths
 
     #print('dfs: ', actions[len(actions)-1])
-    print('dfs', snake.expanded)
-    return average_length
+    #print('dfs', snake.expanded)
+    return average_length, actions[len(actions)-1], snake.expanded
 
 # Breadth-first search algorithm
 def bfs(snake: snake):
@@ -122,8 +122,8 @@ def bfs(snake: snake):
         average_length = total_length / num_paths
 
     #print('bfs: ', actions[len(actions)-1])
-    print('bfs', snake.expanded)
-    return average_length
+    #print('bfs', snake.expanded)
+    return average_length, actions[len(actions)-1], snake.expanded
 
 # Uniform Cost Search algorithm --> all states have the same priority
 def ucs(snake: snake):
@@ -168,8 +168,8 @@ def ucs(snake: snake):
         average_length = total_length / num_paths
 
     #print('ucs: ', actions[len(actions)-1])
-    print('ucs', snake.expanded)
-    return average_length
+    #print('ucs', snake.expanded)
+    return average_length, actions[len(actions)-1], snake.expanded
 
 def astar(snake: snake, heuristic):
     
@@ -212,8 +212,8 @@ def astar(snake: snake, heuristic):
         average_length = total_length / num_paths
 
     #print('astar: ', actions[len(actions)-1])
-    print('astar', snake.expanded)
-    return average_length
+    #print('astar', snake.expanded)
+    return average_length, actions[len(actions)-1], snake.expanded
 
 s = snake((10, 10))
 
@@ -226,40 +226,66 @@ def gen_food_list():
         food = (foodx, foody)
         food_list.append(food)
 
-# gen_food_list()
-# total_average_length_dfs = 0
-# total_average_length_bfs = 0
-# total_average_length_ucs = 0
-# total_average_length_astar = 0
-# astar_euclidean = 0
-# dfs_time = 0
-# num_iterations = len(food_list)
-# for i in range(0, len(food_list)):
-#     average_length_dfs = dfs(s)
-#     average_length_bfs = bfs(s)
-#     average_length_ucs = ucs(s)
-#     average_length_astar = astar(s, manhattanHeuristic)
-#     average_length_euclidean = astar(s, euclideanHeuristic)
+# find the average length of the path snake takes for each search algorithm for all 400 possible locations of food (on a 20x20 board)
+def average_path_length():
+    gen_food_list()
+    total_average_length_dfs = 0
+    total_average_length_bfs = 0
+    total_average_length_ucs = 0
+    total_average_length_astar = 0
+    astar_euclidean = 0
+    num_iterations = len(food_list)
+    for i in range(0, len(food_list)):
+        average_length_dfs = dfs(s)[0]
+        average_length_bfs = bfs(s)[0]
+        average_length_ucs = ucs(s)[0]
+        average_length_astar = astar(s, manhattanHeuristic)[0]
+        average_length_euclidean = astar(s, euclideanHeuristic)[0]
 
-#     total_average_length_dfs += average_length_dfs
-#     total_average_length_bfs += average_length_bfs
-#     total_average_length_ucs += average_length_ucs
-#     total_average_length_astar += average_length_astar
-#     astar_euclidean += average_length_euclidean
-
-
-# print("Average length of DFS:", round(total_average_length_dfs / num_iterations))
-# print("Average length of BFS:", round(total_average_length_bfs / num_iterations))
-# print("Average length of UCS:", round(total_average_length_ucs / num_iterations))
-# print("Average length of A* with manhattan heuristic:", round(total_average_length_astar / num_iterations))
-# print("Average length of A* with euclidean heuristic:", round(astar_euclidean / num_iterations))
+        total_average_length_dfs += average_length_dfs
+        total_average_length_bfs += average_length_bfs
+        total_average_length_ucs += average_length_ucs
+        total_average_length_astar += average_length_astar
+        astar_euclidean += average_length_euclidean
 
 
-# prints out the list of directions each algorithm would take
-# (uncomment lines 256-260 to view these lists as well as the print statements prior to the return statement in each function)
-for i in range(0, 100):
-    dfs(s)
-    bfs(s)
-    ucs(s)
-    astar(s, manhattanHeuristic)
-    astar(s, euclideanHeuristic)
+    print("Average length of DFS:", round(total_average_length_dfs / num_iterations))
+    print("Average length of BFS:", round(total_average_length_bfs / num_iterations))
+    print("Average length of UCS:", round(total_average_length_ucs / num_iterations))
+    print("Average length of A* with manhattan heuristic:", round(total_average_length_astar / num_iterations))
+    print("Average length of A* with euclidean heuristic:", round(astar_euclidean / num_iterations))
+
+# prints the list of actions taken with each search algorithm
+def actions_taken():
+    print(dfs(s)[1])
+    print(bfs(s)[1])
+    print(ucs(s)[1])
+    print(astar(s, manhattanHeuristic)[1])
+    print(astar(s, euclideanHeuristic)[1])
+
+# returns the number of states expanded for 1 iteration of each search algorithm
+def states_expanded():
+    print(dfs(s)[2])
+    print(bfs(s)[2])
+    print(ucs(s)[2])
+    print(astar(s, manhattanHeuristic)[2])
+    print(astar(s, euclideanHeuristic)[2])
+
+
+
+#average_path_length()
+#actions_taken()
+#states_expanded()
+
+# argparse for the user to compare the search algorithms in different ways
+parser = argparse.ArgumentParser()
+parser.add_argument('--comparator', required=True, default='path_length')
+parser.add_argument('help', )
+args = parser.parse_args()
+
+if (args.comparator == 'path_length'):
+    average_path_length()
+elif args.comparator == 'actions':
+    actions_taken()
+elif args.comparator == 'states':
+    states_expanded()
